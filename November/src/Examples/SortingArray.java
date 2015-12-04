9cce63b1641f7261dbdf694a6d3de7c933b1afdf
inpath <vrmana@gmail.com>
2015-12-03 22:56:47 -0800

    Java Programs

---
+++ November/src/Examples/SortingArray.java
@@ -0,0 +1,49 @@
+package Examples;
+
+
+public class SortingArray {
+
+	int min,j;
+	
+	void largest(int[] arr2)
+	{
+		int min = arr2[0];
+		
+		
+		for (j=0;j< arr2.length-1; j++)
+		{
+		/*boolean sort=false;
+		while(sort==false)
+		{
+			sort=true;*/		
+		for (int i = 0;i < arr2.length-1;i++)
+		{
+			if (arr2[i] > arr2[i+1])
+			{
+				min = arr2[i+1];
+				arr2[i+1] = arr2[i];
+				arr2[i]=min;
+				//sort=false;
+			}
+				}
+				
+				}
+	
+			for (int i=0;i<arr2.length;i++)
+		{
+			System.out.println(arr2[i]);
+		}
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		int[] arr1 = {22,111,5,4,3};
+		SortingArray sa = new SortingArray();
+		System.out.println("The ascending order is ");
+		sa.largest(arr1);
+		
+			
+	}
+
+}
